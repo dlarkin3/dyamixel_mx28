cb99cf01d861d31990dbff033a7cee543781e712
User1 Laptop7 <user1@laptop7.com>
2018-07-14 00:11:34 -0400

    oop test1 works

--- dynamixel_mx28.py
+++ dynamixel_mx28.py
@@ -35,7 +35,74 @@ class dynamixel_mx28:
         self.dxl_id = dxl_id
         self.baud = baud
         self.device = device
+                
+        # Initialize PortHandler instance
+        # Get methods and members of PortHandlerLinux
+        self.portHandler = PortHandler(self.device)
+
+        # Initialize PacketHandler instance
+        # Get methods and members of Protocol1PacketHandler
+        self.packetHandler = PacketHandler(PROTOCOL_VERSION)  
         
+        # Open port
+        if self.portHandler.openPort():
+            print("Succeeded to open the port")
+        else:
+            print("Failed to open the port")
+            quit()
+
+        # Set port baudrate
+        if self.portHandler.setBaudRate(self.baud):
+            print("Succeeded to change the baudrate")
+        else:
+            print("Failed to change the baudrate")
+            quit()   
+            
+    def __del__(self):
+        self.portHandler.closePort()    
+            
+    def set_cw_limit(self,limit=0):
+        dxl_comm_result, dxl_error = self.packetHandler.write2ByteTxRx(self.portHandler, self.dxl_id, ADDR_CW_ANGLE_LIMIT, limit)
+        if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
+        elif dxl_error != 0:
+            print("%s" % self.packetHandler.getRxPacketError(dxl_error))
+        else:
+            print("Dynamixel CW limit set to %d" % limit)
+            
+    def set_ccw_limit(self,limit=0):
+        dxl_comm_result, dxl_error = self.packetHandler.write2ByteTxRx(self.portHandler, self.dxl_id, ADDR_CCW_ANGLE_LIMIT, limit)
+        if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
+        elif dxl_error != 0:
+            print("%s" % self.packetHandler.getRxPacketError(dxl_error))
+        else:
+            print("Dynamixel CCW limit set to %d" % limit)
+
+    def set_wheel_mode(self):
+        self.set_cw_limit(0)
+        self.set_ccw_limit(0)
         
+    def get_present_position(self):
+        dxl_position = 99999 # error code
+        dxl_position, dxl_comm_result, dxl_error = self.packetHandler.read2ByteTxRx(self.portHandler, self.dxl_id, ADDR_MX_PRESENT_POSITION)
+        if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
+        elif dxl_error != 0:
+            print("%s" % self.packetHandler.getRxPacketError(dxl_error))  
+        return dxl_position # returns 99999 if not successful else returns valid position
         
+    def set_moving_speed(self,set_speed):
+        dxl_comm_result, dxl_error = self.packetHandler.write2ByteTxRx(self.portHandler, self.dxl_id, ADDR_MX_MOVING_SPEED, set_speed)
+        if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
+        elif dxl_error != 0:
+            print("%s" % self.packetHandler.getRxPacketError(dxl_error))                                
         
+if __name__ == "__main__":
+    dyna1 = dynamixel_mx28(dxl_id=3)
+    dyna2 = dynamixel_mx28(dxl_id=4)   
+    dyna1.set_wheel_mode()
+    dyna2.set_wheel_mode()
+    print("Pos1: %d, Pose2: %d" % (dyna1.get_present_position(), dyna2.get_present_position()))   
+
